03b2793ec8577f219a15ffdcf32dfc9a1691541c
Noé Armando Artero Santacruz <nenearterosantacruz@outlook.com>
2026-03-10 17:41:47 -0600

    Redesign login UI and update app startup flow
    Major overhaul of the login screen: new layout with header split into three panels, centered login form, and gradient styling. Added dynamic centering logic for controls and buttons. "Cancelar" exits app, "Ingresar" opens InicioView. Program now starts with Login form. Added HerramientasTotal namespace reference.

--- ProyectoADS_ROJEX/Login.cs
+++ ProyectoADS_ROJEX/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HerramientasTotal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,61 @@ namespace ProyectoADS_ROJEX
         public Login()
         {
             InitializeComponent();
+        }
+
+
+        //parte para centrar elementos
+        private void CentrarControl(Control control, Control contenedor)
+        {
+            control.Left = (contenedor.Width - control.Width) / 2;
+            control.Top = (contenedor.Height - control.Height) / 2;
+        }
+        private void CentrarBotones()
+        {
+            int espacioEntreBotones = 15;
+
+            int anchoTotal = btnIngresar.Width + btnCancelar.Width + espacioEntreBotones;
+
+            int xInicial = (PanelInferiorBotonesCentro.Width - anchoTotal) / 2;
+            int y = (PanelInferiorBotonesCentro.Height - btnIngresar.Height) / 2;
+
+            btnIngresar.Left = xInicial;
+            btnIngresar.Top = y;
+
+            btnCancelar.Left = btnIngresar.Right + espacioEntreBotones;
+            btnCancelar.Top = y;
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+            CentrarControl(lblTitulo, panelCentro);
+            CentrarControl(lblInicioDeSesion, panelInicioSesionFinal);
+            CentrarBotones();
+        }
+
+        private void Login_Resize(object sender, EventArgs e)
+        {
+            CentrarControl(lblTitulo, panelCentro);
+            CentrarControl(lblInicioDeSesion, panelInicioSesionFinal);
+            CentrarBotones();
+        }
+
+        private void panelCentro_Resize(object sender, EventArgs e)
+        {
+            CentrarControl(lblTitulo, panelCentro);
+            CentrarControl(lblInicioDeSesion, panelInicioSesionFinal);
+            CentrarBotones();
+        }
+        // fin parte para centrar elementos
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            InicioView inicioView = new InicioView();
+            inicioView.Show();
+
         }
     }
 }
--- ProyectoADS_ROJEX/Program.cs
+++ ProyectoADS_ROJEX/Program.cs
@@ -13,7 +13,7 @@ namespace ProyectoADS_ROJEX
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new InicioView());
+            Application.Run(new Login());
         }
     }
 }
